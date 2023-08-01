ebfa2775ee77127d0ec027957d1f028dffc6f234
Siddharth2302 <siddharthrajesh2302@gmail.com>
2023-08-02 02:03:45 +0530

    Find the difference

---
+++ Find the difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin() , t.end());
+        for(int i = 0 ; i<s.length();i++){
+            if(s[i]==t[i]){
+                continue;
+            }
+            else return t[i];
+        }
+        return t[s.length()];
+        
+        
+    }
+};
